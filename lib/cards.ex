4d80a43c30262fd9cf928143e616757ff4b841a0
Roel Kerkhofs <roel@Roels-MacBook-Air.local>
2018-12-22 20:25:53 +0100

    Up until Lecture 12

---
+++ lib/cards.ex
@@ -0,0 +1,31 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+
+    for suit <- suits do
+      suit
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+end
